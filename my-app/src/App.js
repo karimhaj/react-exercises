a83d90de7037b521ba5dfa194f2f5260dc624933
Karim Hajouan <101986559+karimhaj@users.noreply.github.com>
2022-07-16 11:04:28 +0200

    add App component

---
+++ my-app/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Hello } from './HelloWorld';
+
+export class App extends React.Component {
+    render(){
+        return(
+            <div>
+                <Hello />
+            </div>
+        )
+    }
+}
