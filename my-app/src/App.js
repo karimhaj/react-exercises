a4cf3ae924d53e6c3beb51cfaea588bafb2d1a87
Karim Hajouan <101986559+karimhaj@users.noreply.github.com>
2022-07-18 21:13:26 +0200

    exercise 4 of props

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import { Hello } from './HelloWorld';
 import { Welcome } from './Message';
 
+const theName = <b>Clark</b>;
+
 export class App extends React.Component {
     render(){
         return(
             <div>
                 <Hello />
-                <Welcome age='20' />
+                <Welcome age='20' name={theName}/>
             </div>
         )
     }
