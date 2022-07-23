ebadf2118333c4b39270961364667bbfa337e89e
Karim Hajouan <101986559+karimhaj@users.noreply.github.com>
2022-07-23 13:49:10 +0200

    complete exercise 4 of conditional rendering

--- my-app/src/Message.js
+++ my-app/src/Message.js
@@ -13,9 +13,9 @@ export class Welcome extends React.Component {
         <p>welcome {this.props.name}!</p>
 
         {
-        this.props.age && this.props.age > 18 && this.props.age < 65
+        this.props.age > 18 && this.props.age < 65 && this.props.name === 'John'
         ?<Age age = {this.props.age}/>
-        : <p>age is not between 19 and 64</p>
+        : <p>age is not between 19 and 64 or the name is not John</p>
         }
 
         </div>
@@ -38,5 +38,5 @@ export class Age extends React.Component{
 }
 
 Welcome.defaultProps = {
-    name: 'Homie'
+    name: 'john'
 }; 
