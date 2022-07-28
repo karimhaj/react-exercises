ce61447b181b60d18e8e0edfb539bad15646b060
Karim Hajouan <101986559+karimhaj@users.noreply.github.com>
2022-07-28 10:16:15 +0200

    complete exercise 3 of forms

--- my-app/src/InteractiveWelcome.js
+++ my-app/src/InteractiveWelcome.js
@@ -11,24 +11,47 @@ export class InteractiveWelcome extends React.Component{
         })
         return console.log(this.state);
     }
-
+    
     render(){
         return (
             <div>
                 <Welcome name={this.state.username}/>
                 <input type= "text" name= "username" value={this.state.username} onChange={this.handleUsernameInputChange}/>
+                <Login onLogin={this.state} />
             </div>
         )
     }
 }
 
 export class Login extends React.Component{
+
+    state= {
+        username: "",
+        password: "",
+        remember: ""
+    }
+
+    handleUsernameInputChange = (event) =>{
+        let name = event.target.name;
+
+        this.setState({
+            [name]: event.target.value,
+            compiled: this.state.username !== "" && this.state.password !== "" ? true : false
+        })
+    }
+
+    onLogin = () =>{
+       return console.log(this.state)
+    }
+
     render(){
         return (
             <div>
-                <input type= "text" name= "username" value= ""  />
-                <input type= "paassword" name= "password" value= ""  />
-                <input type= "checkbox" name= "remember" value= ""  />
+                <h2>LOGIN</h2>
+                <input type= "text" name= "username" value={this.state.username} onChange={this.handleUsernameInputChange}/>
+                <input type= "password" name= "password" value={this.state.password} onChange={this.handleUsernameInputChange} />
+                <input type= "checkbox" name= "remember" value={this.state.remember}  />
+                <button disabled={!this.state.compiled} onClick={this.onLogin}>Login</button>
             </div>
         )
     }
