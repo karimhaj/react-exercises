cc1a1ac64912556e664ab436707971571e72e50b
Karim Hajouan <101986559+karimhaj@users.noreply.github.com>
2022-07-29 21:40:47 +0200

    initial commit

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,19 +1,10 @@
 import React from "react";
-import { Hello } from './HelloWorld';
-import { Welcome } from './Message';
-import Counter from "./Counter";
-import { InteractiveWelcome } from "./InteractiveWelcome";
-
-const theName = <b>Clark</b>;
 
 export class App extends React.Component {
     render(){
         return(
             <div>
-                <Hello />
-                <Welcome age="19" name={theName}/>
-                <Counter value={0} incrementInterval={1000} incrementAmount={2} />
-                <InteractiveWelcome />
+
             </div>
         )
     }
