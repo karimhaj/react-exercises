284c84e84a9fda1a595e27917e9f5bc4ac4d5ac2
Karim Hajouan <101986559+karimhaj@users.noreply.github.com>
2022-07-31 14:33:43 +0200

    complete exercise 3 of lists

---
+++ my-app/src/TodoList.js
@@ -0,0 +1,34 @@
+import React from "react";
+
+export class TodoList extends React.Component{
+
+  state ={
+    items: ['Lucas', 'Sandra', 'Jude'],
+    name: ""
+    }
+
+    handleControlledNameChange = (event) =>{
+        this.setState ({
+            name: event.target.value
+        })
+        return console.log(this.state)
+    }
+
+    addElementToList = (event) =>{
+        event.preventDefault() 
+
+        this.setState({
+            items: [...this.state.items, this.state.name]
+        })
+    }
+
+    render(){
+        return (<div>
+        <ul>{this.state.items.map((item, index)=> <li key={index}>{item}</li>)}</ul>
+        <form onSubmit={this.addElementToList}>
+        <input type="text" name ="newItem" value={this.state.name} onChange={this.handleControlledNameChange}/>
+        <button type="submit" name="addToList">Add to list</button>
+        </form>
+        </div>)
+    }
+}
