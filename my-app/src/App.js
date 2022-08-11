9b945d2c476d4f9994eb898c2d1bee65a0f54b6f
Karim Hajouan <101986559+karimhaj@users.noreply.github.com>
2022-08-11 12:42:13 +0200

    complete exercise 1 of function-components

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,50 +1,12 @@
 import React from "react";
-import { Hello } from './HelloWorld';
-import { Welcome } from './Message';
-import Counter from "./Counter";
-import { InteractiveWelcome } from "./InteractiveWelcome";
-import { TodoList } from "./TodoList";
-import "./index.css"
-import { Container } from "./Container";
-import DisplayLanguage from "./DisplayLanguage";
-import { LanguageContext } from "./LanguageContext";
 import { Welcome2 } from "./Welcome2";
 
-const theName = <b>Clark</b>;
-
 export class App extends React.Component {
-
-    state = {
-        language: 'en'
-    }
-
-    handleLanguageChange = (event) => {
-        this.setState({
-            language: event.target.value
-        })
-    }
-
-
 
     render() {
         return (
             <div>
-                <select value={this.state.value} onChange={this.handleLanguageChange}>
-                <option value='en'>ENGLISH</option>
-                <option value='it'>ITALIANO</option>
-            </select>
-                <Welcome2 name="Jimmy" />
-                <Container title="This awesome application">
-                    <hr/>
-                <LanguageContext.Provider value={this.state.language}>
-                    <DisplayLanguage />
-                    </LanguageContext.Provider> 
-                    <Hello />
-                    <Welcome age="19" name={theName} />
-                    <Counter value={0} incrementInterval={1000} incrementAmount={2} />
-                    <InteractiveWelcome />
-                    <TodoList /> 
-                </Container>
+                <Welcome2 name="Erik" />
             </div>
         )
     }
--- my-app/src/Welcome.js
+++ my-app/src/Welcome.js
@@ -2,6 +2,6 @@ import React from "react";
 
 export function Welcome2 (props){
     return (<div>
-        <h2>Hello {props.name}</h2>
+        <h2>Hello {props.name} !</h2>
     </div>)
 }
