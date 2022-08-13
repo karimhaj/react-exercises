45f5ea43e6ac005816f67662483b0d6e4fb078b4
Karim Hajouan <101986559+karimhaj@users.noreply.github.com>
2022-08-13 20:49:31 +0200

    complete exercise 2 of use state

--- my-app/src/InteractiveWelcome.js
+++ my-app/src/InteractiveWelcome.js
@@ -1,58 +1,32 @@
-import React from "react";
-import { Welcome } from "./Message";
+import React, { useState } from "react";
 
-export class InteractiveWelcome extends React.Component{
-
-    state ={ username: '' }
-    
-    handleUsernameInputChange = (event) =>{
-        this.setState({
-            username: event.target.value
-        })
-        return console.log(this.state);
-    }
-    
-    render(){
-        return (
-            <div>
-                <Welcome name={this.state.username}/>
-                <input type= "text" name= "username" value={this.state.username} onChange={this.handleUsernameInputChange}/>
-                <Login onLogin={this.state} />
-            </div>
-        )
-    }
-}
-
-export class Login extends React.Component{
-
-    state= {
+export function Login (props){
+    const [data, setData] = useState({
         username: "",
         password: "",
-        remember: ""
+        remember: false
+    })
+
+    function handleInputChange (event){
+       const {name, type, value, checked} = event.target
+
+       setData({
+        ...data,
+        [name]: type === 'checkbox' ? checked : value
+       })
     }
 
-    handleUsernameInputChange = (event) =>{
-        let name = event.target.name;
-
-        this.setState({
-            [name]: event.target.value,
-            compiled: this.state.username !== "" && this.state.password !== "" ? true : false
-        })
+    function handleLogin(event){
+        event.preventDefault()
+        console.log(data)
     }
+ 
 
-    onLogin = () =>{
-       return console.log(this.state)
-    }
-
-    render(){
-        return (
-            <div>
-                <h2>LOGIN</h2>
-                <input type= "text" name= "username" value={this.state.username} onChange={this.handleUsernameInputChange}/>
-                <input type= "password" name= "password" value={this.state.password} onChange={this.handleUsernameInputChange} />
-                <input type= "checkbox" name= "remember" value={this.state.remember}  />
-                <button disabled={!this.state.compiled} onClick={this.onLogin}>Login</button>
-            </div>
-        )
-    }
+    return(<form>
+        <h1>Login</h1>
+        <input onChange={handleInputChange} type="text" name="username" value={data.username} />
+        <input onChange={handleInputChange} type="password" name="password" value={data.password}/>
+        <input onChange={handleInputChange} type="checkbox" name="remember" value={data.remember}/>
+        <button onClick={handleLogin}>Login</button>
+        </form>)
 }
