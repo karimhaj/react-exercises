b4f0972469fdbabcee7612ce08ded5f2ebdb4a19
Karim Hajouan <101986559+karimhaj@users.noreply.github.com>
2022-08-14 15:55:32 +0200

    update exercise 3 of function components

--- my-app/src/Sum.js
+++ my-app/src/Sum.js
@@ -1,10 +1,6 @@
 import React from 'react';
 
-export function Sum({...props}){
-    let result = props.numbers.reduce((current,next)=> current + next,0)
+export function Sum({numbers = [0,1]}){
+    let result = numbers.reduce((current,next)=> current + next,0)
     return(<div><h1>{result}</h1></div>)
-}
-
-Sum.defaultProps={
-    numbers: [3,5]
 }
