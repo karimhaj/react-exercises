b0a20efb28dd309c01abcfd775ac4f49ec480770
Karim Hajouan <101986559+karimhaj@users.noreply.github.com>
2022-08-15 20:14:41 +0200

    complete exercise 2 of use effect

--- my-app/src/Counter.js
+++ my-app/src/Counter.js
@@ -3,19 +3,11 @@ import React, { useEffect, useState } from 'react';
 export function Counter({initialValue = 0, onCounterChange}){
     const [counter, setCounter] = useState(initialValue);
 
-    useEffect(()=>{onCounterChange(counter)}, [counter,onCounterChange])
-
-    function handleCounterIncrement(){
-        setCounter(c => c + 1)
-    }
-
-    function handleCounterReset(){
-        setCounter(initialValue)
-    }
+    useEffect(()=>{
+        const intervalCounter = setInterval(()=>{ setCounter(counter + 1)},1000)
+        return () =>{clearInterval(intervalCounter)}}, [counter])
 
     return(<div>
         <h1>Counter: {counter}</h1>
-        <button onClick={handleCounterIncrement}>increment counter</button>
-        <button onClick={handleCounterReset}>reset</button>
         </div>)
 }
