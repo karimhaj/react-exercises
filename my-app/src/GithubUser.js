fcae1dc0eb92500ec96b77c2e96304500388ae8f
Karim Hajouan <101986559+karimhaj@users.noreply.github.com>
2022-08-19 11:30:01 +0200

    complete exercise 4 of custom hooks

--- my-app/src/GithubUser.js
+++ my-app/src/GithubUser.js
@@ -1,12 +1,16 @@
 import { useGithubUser } from "./useGithubUser";
 
 export function GithubUser({name}){
-    const username = useGithubUser(name)
+    const {username , error, loading } = useGithubUser(name)
 
-    return(<div>
-        <h2>{username.name}</h2>
-        <h3>{username.id}</h3>
-        <h3>{username.login}</h3>
-        <img src={username.avatar_url} alt="github profile pic" />
-        </div>)
+    return(<>
+        {loading && <h1>Loading...</h1>}
+        {error && <h1>An error has occurred</h1>}
+        {username && <div>
+                <h2>{username.name}</h2>
+                <h3>{username.id}</h3>
+                <h3>{username.login}</h3>
+                <img src={username.avatar_url} alt="github profile pic" />
+                </div>}
+        </>)
 }
--- my-app/src/useGithubUser.js
+++ my-app/src/useGithubUser.js
@@ -2,12 +2,22 @@ import { useState, useEffect } from "react";
 
 export function useGithubUser(name){
     const [username, setUsername] = useState({});
+    const [error, setError] = useState(null);
+    const [loading, setLoading] = useState(null);
 
     useEffect(()=>{
+        setLoading(true);
+        setError(false)
         fetch(`https://api.github.com/users/${name}`)
         .then(res => res.json())
         .then(data=> setUsername(data))
-    },[name])
+        .catch(error => setError(true), setUsername(false))
+        setLoading(false)
+    },[name,error])
 
-    return username
+    return {
+        username,
+        error,
+        loading
+    }
 }
