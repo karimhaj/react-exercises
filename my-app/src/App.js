cd18a17aec15fedcd3a9894edcfa4ec89db4bab4
Karim Hajouan <101986559+karimhaj@users.noreply.github.com>
2022-08-21 08:56:36 +0200

    clear files after completing es 1 of re act router

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,5 +1,4 @@
 import { Route, Routes, Link } from "react-router-dom";
-import { Counter } from "./Counter";
 import { ShowGithubUser } from "./ShowGithubUser";
 import { Welcome } from "./Welcome";
 
@@ -11,7 +10,6 @@ export function App() {
         <Link to="/users/:username">githubUser</Link>
       <Routes>
         <Route path="/" element={<Welcome name="Erik"/>} />
-        <Route path="/counter" element={<Counter/>} />
         <Route path="users/:username" element={<ShowGithubUser />} />
       </Routes>
     </div>
